154d1af4fa9cfb97336e03da4d30bb6fcfd1406d
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-12-05 18:23:43 +0300

    Binded DataList and hardcoded the data

--- WpfToDoApp/MainWindow.xaml.cs
+++ WpfToDoApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfToDoApp.Models;
 
 namespace WpfToDoApp
 {
@@ -20,9 +22,57 @@ namespace WpfToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<ToDoModel> _toDoDataList;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _toDoDataList = new BindingList<ToDoModel>()
+            {
+                new ToDoModel() { Text = "test" },
+                new ToDoModel() { Text = "some string" },
+                new ToDoModel() { Text = "task 3", IsDone = true }
+            };
+
+            ToDoList.ItemsSource = _toDoDataList;
+
+            _toDoDataList.ListChanged += _toDoDataList_ListChanged;
+        }
+
+        private void _toDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || 
+                e.ListChangedType == ListChangedType.ItemDeleted ||
+                e.ListChangedType == ListChangedType.ItemChanged)
+            {
+
+            }
+
+            //switch (e.ListChangedType)
+            //{
+            //    case ListChangedType.Reset:
+            //        break;
+            //    case ListChangedType.ItemAdded:
+            //        break;
+            //    case ListChangedType.ItemDeleted:
+            //        break;
+            //    case ListChangedType.ItemMoved:
+            //        break;
+            //    case ListChangedType.ItemChanged:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorAdded:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorDeleted:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorChanged:
+            //        break;
+            //    default:
+            //        break;
+            //}
         }
     }
 }
---
+++ WpfToDoApp/Models/ToDoModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WpfToDoApp.Models
+{
+    class ToDoModel : INotifyPropertyChanged
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _text;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set 
+            {
+                if (_isDone == value)
+                    return;
+                _isDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+        
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text == value)
+                    return;
+                _text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = "")
+        {
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            //}
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
